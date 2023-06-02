a1faab0f689b863ddf04a4ce7851ac5d9d8c4790
Andrey <baev2001@yandex.ru>
2023-06-02 10:27:42 +0300

    commit

---
+++ second_task.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+import pandas as pd
+import requests
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2021, 1, 1)
+}
+
+dag = DAG(
+    'api_to_csv_dag',
+    default_args=default_args,
+    schedule_interval="@once",
+)
+
+def load_data():
+    response = requests.get('https://api.publicapis.org/entries')
+    data = response.json()['entries']
+    df = pd.DataFrame(data)
+    df.to_csv('/tmp/data.csv', header=False, index=False)
+    return '/tmp/data.csv'
+
+load_data_task = PythonOperator(
+    task_id='load_data',
+    python_callable=load_data,
+    dag=dag,
+)
+
+def print_data(**context):
+    file_path = context['task_instance'].xcom_pull(task_ids='load_data')
+    df = pd.read_csv(file_path, header=None)
+    print(df)
+
+print_data_task = PythonOperator(
+    task_id='print_data',
+    python_callable=print_data,
+    provide_context=True,
+    dag=dag,
+)
+
+load_data_task >> print_data_task
